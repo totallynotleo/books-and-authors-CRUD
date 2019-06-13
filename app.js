14d69f8ee1ecb55a8c8f9b8b784039c9ee02b5ca
totallynotleo <leodabrim@gmail.com>
2019-06-13 11:33:29 +1000

    Installed fileUpload for adding photos

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const port = 3000;
+
+app.use(fileUpload());
 
 mongoose.connect('mongodb://127.0.0.1/books_r_us', { useNewUrlParser: true });
 mongoose.connection.on('error', err => console.log(err));
